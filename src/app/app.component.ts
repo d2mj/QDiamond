ad1064b84619e5ff9f10ef101b52e92a1bc6c5f7
d2mj <dj.d2mj@gmail.com>
2018-09-27 01:26:10 +0530

    Added score on game completion and added start stop actions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,19 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
   size = [0, 1, 2, 3, 4, 5, 6, 7];
   diamondColPos = [];
   diamondRowPos = [];
+  diamondCount = 0;
+  cellVisitedCount = 0;
+  showGameBoard = true;
 
   ngOnInit() {
+    this.placeDiamondsIntoCells();
+  }
+
+  placeDiamondsIntoCells() {
     while (this.diamondColPos.length < 8) {
       const randomnumber = Math.floor(Math.random() * 8);
       if (this.diamondColPos.indexOf(randomnumber) > -1) {
@@ -29,5 +35,32 @@ export class AppComponent {
     }
     console.log('diamond row: ', this.diamondRowPos);
     console.log('diamond col: ', this.diamondColPos);
+  }
+
+  dCount(e) {
+    this.diamondCount = this.diamondCount + 1;
+    console.log(e, this.diamondCount);
+  }
+
+  visitCount(e) {
+    this.cellVisitedCount = this.cellVisitedCount + 1;
+    console.log('diamond count ', this.diamondCount, 'visited cells ', this.cellVisitedCount);
+    if (this.diamondCount === 8) {
+      alert(`Congratulations, you found all diamonds and your score is ${64 - this.cellVisitedCount}`);
+      this.stop();
+    }
+  }
+
+  start() {
+    this.showGameBoard = true;
+    this.diamondColPos = [];
+    this.diamondRowPos = [];
+    this.diamondCount = 0;
+    this.cellVisitedCount = 0;
+    this.placeDiamondsIntoCells();
+  }
+
+  stop() {
+    this.showGameBoard = false;
   }
 }
